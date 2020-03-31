e2738a665ea37363c4a332a8f4f344beaf2eb99c
kcelik <kaan.celik@nar.com.tr>
2020-04-01 01:49:17 +0300

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const { httpAdapter } = app.get(HttpAdapterHost);
+  app.enableCors();
   app.useGlobalFilters(new AllExceptionFilter(httpAdapter));
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
